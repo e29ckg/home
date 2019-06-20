69208f2ce88634d9c2672411a225cffa11f07142
e29ckg <e29ckg@gmail.com>
2019-06-20 14:54:35 +0700

    Update main.php

--- views/site/main.php
+++ views/site/main.php
@@ -13,63 +13,15 @@ $this->params['breadcrumbs'][] = $this->title;
 <link href="<?= Url::to('@web/plugins/fullcalendar/packages/core/main.css')?>" rel='stylesheet' />
 <link href="<?= Url::to('@web/plugins/fullcalendar/packages/daygrid/main.css')?>" rel='stylesheet' />
 <link href="<?= Url::to('@web/plugins/fullcalendar/packages/list/main.css')?>" rel='stylesheet' />
-<script src="<?= Url::to('@web/plugins/fullcalendar/packages/core/main.js')?>"></script>
-<script src="<?= Url::to('@web/plugins/fullcalendar/packages/interaction/main.js')?>"></script>
-<script src="<?= Url::to('@web/plugins/fullcalendar/packages/daygrid/main.js')?>"></script>
-<script src="<?= Url::to('@web/plugins/fullcalendar/packages/list/main.js')?>"></script>
-<script src="<?= Url::to('@web/plugins/fullcalendar/packages/google-calendar/main.js')?>"></script>
-<script>
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],   
-		lang: 'th',
-      plugins: [ 'interaction', 'dayGrid', 'list', 'googleCalendar' ],
-
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,listYear'
-      },
-
-      displayEventTime: false, // don't show the time column in list view
-
-      // THIS KEY WON'T WORK IN PRODUCTION!!!
-      // To make your own Google API key, follow the directions here:
-      // http://fullcalendar.io/docs/google_calendar/
-      googleCalendarApiKey: 'AIzaSyCtp0KVVxbk9VapZoU-X4J6uaulYafzMQw',
-
-      // US Holidays
-      events: 'pkkjc.coj@gmail.com',
-
-      eventClick: function(arg) {
-        // opens events in a popup window
-        window.open(arg.event.url, 'google-calendar-event', 'width=800,height=600');
-
-        arg.jsEvent.preventDefault() // don't navigate in main tab
-      },
-
-      loading: function(bool) {
-        document.getElementById('loading').style.display =
-          bool ? 'block' : 'none';
-      }
-
-    });
-
-    calendar.render();
-  });
-
-</script>
 <style>
 
-  body {
+  /* body {
     margin: 40px 10px;
     padding: 0;
     font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
     font-size: 14px;
-  }
+  } */
 
   #loading {
     display: none;
@@ -109,7 +61,7 @@ $this->params['breadcrumbs'][] = $this->title;
 		</ul>
 	</div>
 </div>
-<div class="row">
+<div class="">
     <!-- widget grid -->
 	<section id="widget-grid" class="">
 				
@@ -167,10 +119,7 @@ $this->params['breadcrumbs'][] = $this->title;
 	<!-- <iframe src="https://calendar.google.com/calendar/embed?src=pkkjc.coj%40gmail.com&ctz=Asia%2FBangkok" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe> -->
 </div>
 
-
-
-  
-			<div class="col-sm-12 col-md-12 col-lg-12">
+	<div class="col-sm-12 col-md-12 col-lg-12">
 				
 				<!-- new widget -->
 				<div class="jarviswidget jarviswidget-color-blueDark">
@@ -216,6 +165,54 @@ $this->params['breadcrumbs'][] = $this->title;
 <div>
 .....
 </div>
+<script src="<?= Url::to('@web/plugins/fullcalendar/packages/core/main.js')?>"></script>
+<script src="<?= Url::to('@web/plugins/fullcalendar/packages/interaction/main.js')?>"></script>
+<script src="<?= Url::to('@web/plugins/fullcalendar/packages/daygrid/main.js')?>"></script>
+<script src="<?= Url::to('@web/plugins/fullcalendar/packages/list/main.js')?>"></script>
+<script src="<?= Url::to('@web/plugins/fullcalendar/packages/google-calendar/main.js')?>"></script>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],   
+		lang: 'th',
+      plugins: [ 'interaction', 'dayGrid', 'list', 'googleCalendar' ],
+
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,listYear'
+      },
+
+      displayEventTime: false, // don't show the time column in list view
+
+      // THIS KEY WON'T WORK IN PRODUCTION!!!
+      // To make your own Google API key, follow the directions here:
+      // http://fullcalendar.io/docs/google_calendar/
+      googleCalendarApiKey: 'AIzaSyCtp0KVVxbk9VapZoU-X4J6uaulYafzMQw',
+
+      // US Holidays
+      events: 'pkkjc.coj@gmail.com',
+
+      eventClick: function(arg) {
+        // opens events in a popup window
+        window.open(arg.event.url, 'google-calendar-event', 'width=800,height=600');
+
+        arg.jsEvent.preventDefault() // don't navigate in main tab
+      },
+
+      loading: function(bool) {
+        document.getElementById('loading').style.display =
+          bool ? 'block' : 'none';
+      }
+
+    });
+
+    calendar.render();
+  });
+
+</script>
 <?php
 
 $script = <<< JS
